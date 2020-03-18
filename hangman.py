f9fcb6dcdf81d3b23dc5f8fa70a6801267e06031
bengroves2004 <52626900+bengroves2004@users.noreply.github.com>
2020-03-18 23:11:31 +0000

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -127,22 +127,21 @@ _|___
 
 def createWord():
     global word
-    randomNum = random.randint(0,len(lines))
-    word = lines[randomNum]#selects a random number
-    word = word[:-1]#This removes the \n after the word
+    word = random.choice(lines)  # selects a random number
+    word = word.strip()  # This removes the \n after the word
 
 
 
 
-def createGuessesLog():#This func makes a log which records
+def createGuessesLog():  # This func makes a log which records
     global guessesLog,guessed
                            #the user's correct guesses
     guessesLog=''
 
     for _ in range(len(word)):
         
-        guessesLog += '-'#makes it as long as the word
-    guessesLog = list(guessesLog)#Turns it into an array because it was a str
+        guessesLog += '-'  # makes it as long as the word
+    guessesLog = list(guessesLog)  # Turns it into an array because it was a str
     guessed = '' #As this is only called at the start, there have been no guesses
 
 
@@ -150,23 +149,23 @@ def printTable():
     global incorrect
 
         
-    print(fullTable[incorrect-1])#Prints the current hangman stage
+    print(fullTable[incorrect-1])  # Prints the current hangman stage
 
-    if incorrect > 8:#There are 9 stages untill hanged
+    if incorrect > 8:  # There are 9 stages untill hanged
         gameOver()
 
 
 
 def guess():
     global word, guessesLog,incorrect,guessed,joinedGuessLog
-    #Global allows the variables to be used in other funcs
+      # Global allows the variables to be used in other funcs
     
-    joinedGuessLog = "".join(guessesLog)#Puts the guessLog back into a str
+    joinedGuessLog = "".join(guessesLog)  # Puts the guessLog back into a str
 
     while True:
         print(joinedGuessLog)
         guess = input(f'\nWhat is your guess? Guessed: {guessed}\n')
-        #f''allows variables to put in {}
+          # f''allows variables to put in {}
         
         if guess in guessed:
             print('You already guessed that!\n')
@@ -180,22 +179,22 @@ def guess():
     checker = False
     for i in range(len(word)):
         if word[i] == guess:
-            guessesLog[i]=guess #Add the correctly guessed letter to guessed
+            guessesLog[i]=guess  # Add the correctly guessed letter to guessed
             
-            checker = True #There has been a correct guess
+            checker = True  # There has been a correct guess
 
-        joinedGuessLog = "".join(guessesLog)#Puts the guessLog back into a str
+        joinedGuessLog = "".join(guessesLog)  # Puts the guessLog back into a str
             
-    if checker is True:#If there wasn't a correctly guessed word
-                       #using 'is' is better because it is a boolean
+    if checker is True:  # If there wasn't a correctly guessed word
+                         # using 'is' is better because it is a boolean
                 
          print('Correct!\n')
     else:
 
         print('Incorrect!\n')
         incorrect+=1
-        printTable()#Print the hangman
-    guessed += guess #Add the guess to guessed words
+        printTable()  # Print the hangman
+    guessed += guess  # Add the guess to guessed words
     guessed += ','     
 
 
