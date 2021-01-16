df89a64cf1e59dede4694cd130c4d2db0bdd674c
robinhood <robin.schellemans174@gmail.com>
2021-01-16 17:38:43 +0100

    token inside token.txt and that file in .gitignore

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import discord
 from discord.ext import commands
 
 
-token = "[TOKEN HERE]"
+#  make a text file with token.txt as name and put your bot token in there
+#  and put that file in your .gitignore file, otherwise you will get issues with the bot
+#   also we can both run the scripts for different bots then
+token = open("token.txt", "r").read()
+
 prefix = "-"
 
 client = commands.Bot(command_prefix=prefix)
@@ -11,4 +15,8 @@ client = commands.Bot(command_prefix=prefix)
 async def on_ready():
     print("bot is ready!")
 
-client.run(token) #this line has to be at the end of the code
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+client.run(token) 
