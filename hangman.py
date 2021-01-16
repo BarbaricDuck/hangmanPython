46f0ceb0ab50f10c11c61e9f18490422cfc702f2
bengroves2004 <ben_groves2004@outlook.com>
2021-01-16 15:48:44 +0000

    .

--- hangman.py
+++ hangman.py
@@ -1,224 +1,78 @@
+import random 
+
+def clear():
+    print('\n'*20)
+
 with open('Words-en.txt', 'r') as textFile:
     lines = textFile.readlines() 
 
-import random 
-
-fullTable = [
-r"""
-  
- 
- 
- 
- 
- 
-
-_____
-
-""",
-             
-r"""
-  
- |
- |     
- |      
- |       
- |      
- |
- |
-_|___
-
-
-""",
-             
-r"""
-  
- |/     
- |      
- |      
- |       
- |      
- |
- |
-_|___
-
-""",
-             
-r"""
-  _______
- |/      
- |      
- |     
- |       
- |      
- |
- |
-_|___
-
-""",
-             
-r"""
-  _______
- |/      |
- |      
- |      
- |      
- |      
- |
- |
-_|___
-
-""",
-             
-r"""
-  _______
- |/      |
- |      (_)
- |      
- |       
- |      
- |
- |
-_|___
-
-""",
-             
-r"""
-  _______
- |/      |
- |      (_)
- |       |
- |       |
- |      
- |
- |
-_|___
-
-""",
-r"""
-  _______
- |/      |
- |      (_)
- |      \|/
- |       |
- |
- |
- |
-_|___
-
-""",
-r"""
-  _______
- |/      |
- |      (_)
- |      \|/
- |       |
- |      / \
- |
- |
-_|___
-
-"""]
-
-
-
-
-
-############### --Functions-- ###############
+with open('fullTable.txt','r') as fullTableFile:
+    fullTable = fullTableFile.read().split(',')
 
 def createWord():
-    global word
     word = random.choice(lines)  # selects a random number
     word = word.strip()  # This removes the \n after the word
+    return word
 
-
-
-
-def createGuessesLog():  # This func makes a log which records
-    global guessesLog,guessed
-                           #the user's correct guesses
-    guessesLog=''
-
+def createcorrectGuesses(word):  
+    correctGuesses = []
     for _ in range(len(word)):
-        
-        guessesLog += '-'  # makes it as long as the word
-    guessesLog = list(guessesLog)  # Turns it into an array because it was a str
-    guessed = '' #As this is only called at the start, there have been no guesses
+        correctGuesses.append('-')
+    return correctGuesses
 
-
-def printTable():
-    global incorrect
-
-        
+def printTable(incorrect,word):
     print(fullTable[incorrect-1])  # Prints the current hangman stage
+    if incorrect > 8:  # There are 9 stages until hanged
+        gameOver(word)
 
-    if incorrect > 8:  # There are 9 stages untill hanged
-        gameOver()
+def guess(correctGuesses,guessed,word,incorrect):
+    guessedStr = ''.join(guessed)    
+    print(''.join(correctGuesses))
+    guess = input(f'What is your guess? Guessed: {guessedStr}\n')
+    while guess in guessed:
+        print('You already guessed that!')
+        guess = input(f'What is your guess? Guessed: {guessedStr}\n')   
 
-
-
-def guess():
-    global word, guessesLog,incorrect,guessed,joinedGuessLog
-      # Global allows the variables to be used in other funcs
-    
-    joinedGuessLog = "".join(guessesLog)  # Puts the guessLog back into a str
-
-    while True:
-        print(joinedGuessLog)
-        guess = input(f'\nWhat is your guess? Guessed: {guessed}\n')
-          # f''allows variables to put in {}
-        
-        if guess in guessed:
-            print('You already guessed that!\n')
-            continue
-        
-        break
-
-
-
-    
+    guessed.append(guess)  # Add the guess to guessed words     
+    clear()
     checker = False
-    for i in range(len(word)):
-        if word[i] == guess:
-            guessesLog[i]=guess  # Add the correctly guessed letter to guessed
-            
-            checker = True  # There has been a correct guess
-
-        joinedGuessLog = "".join(guessesLog)  # Puts the guessLog back into a str
-            
-    if checker is True:  # If there wasn't a correctly guessed word
-                         # using 'is' is better because it is a boolean
-                
+    for i,v in enumerate(word):
+        if v == guess:
+            correctGuesses[i]=guess  # Add the correctly guessed letter to guessed           
+            checker = True            
+    if checker is True:             
          print('Correct!\n')
+         return 'correct'
     else:
-
         print('Incorrect!\n')
-        incorrect+=1
-        printTable()  # Print the hangman
-    guessed += guess  # Add the guess to guessed words
-    guessed += ','     
+        return 'incorrect'
+          
+    
+        
 
 
-def checkWin():
-    global word, joinedGuessLog
-    if word == joinedGuessLog:
+def checkWin(word,correctGuesses):
+    
+    if word == ''.join(correctGuesses):
         print(f'Congratulations, The word was {word}')
         repeat()
 
-def gameOver():
+def gameOver(word):
     print(f'Computer Won! The word was {word}')
     repeat()
 
-        
-
 def main():
-    global word, guessesLog, incorrect, guessed
     incorrect = 0
-    createWord()
-    createGuessesLog()
+    guessed = []
+    word = createWord()
+    correctGuesses = createcorrectGuesses(word)
 
     while True:
-        guess()
-        checkWin()
+        if guess(correctGuesses,guessed,word,incorrect) == 'incorrect':
+            incorrect += 1
+            printTable(incorrect,word)
+        
+        checkWin(word,correctGuesses)
 
         
         
@@ -228,7 +82,7 @@ def repeat():
         main()
     exit()
 
-############### --Call main()-- ###############
+
 
 main()
 
